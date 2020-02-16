33b13a60aeb9a27b7e7a3e32b197901d6aa43402
NoohAlavi <noohalavi@gmail.com>
2020-02-16 15:27:52 -0500

    Update Enemy.gd

--- WGJ136/Enemy/Enemy.gd
+++ WGJ136/Enemy/Enemy.gd
@@ -8,5 +8,25 @@ export var follow_speed: float
 export var damage: float
 export var max_health: float
 export var type: String
+export var frozen: bool
 
 onready var health: float = max_health
+
+var target_player: Player
+
+func _ready() -> void:
+	randomize()
+	target_player = player1 if randf() > 0.5 else player2
+	connect("area_entered", self, "on_Enemy_area_entered")
+	player1.connect("died", self, "queue_free")
+	player2.connect("died", self, "queue_free")
+
+func _physics_process(delta: float) -> void:
+	if frozen == true:
+		return
+	if target_player:
+		position += position.direction_to(target_player.position) * delta * follow_speed
+		var bodies = get_overlapping_bodies()
+		for body in bodies:
+			if body is Player:
+				body.health -= 1
