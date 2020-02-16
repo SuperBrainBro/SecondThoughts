d4dfee322964bd1845903349161c950a87ae4f7e
NoohAlavi <noohalavi@gmail.com>
2020-02-16 15:23:36 -0500

    starting new enemy system
    reworking enemy script

--- WGJ136/Enemy/Enemy.gd
+++ WGJ136/Enemy/Enemy.gd
@@ -1,59 +1,16 @@
 extends Area2D
 class_name Enemy
 
-onready var player1: Player = $"../../PlayerHolder/Player1"
-onready var player2: Player = $"../../PlayerHolder/Player2"
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
 
-export var follow_speed: float = 30
-export var frozen: bool = false
-export var type: String = "ogre"
-export var max_health: float = 100
 
-export var health: float = max_health
-export var damage: float = 1 #damage per frame
-onready var spr: Sprite = $Sprite
-
-var target_player: Player
-
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	randomize()
-	target_player = player1 if randf() > 0.5 else player2
-	connect("area_entered", self, "on_Enemy_area_entered")
-	player1.connect("died", self, "queue_free")
-	player2.connect("died", self, "queue_free")
+	pass # Replace with function body.
 
-func _process(delta: float) -> void:
-	match(type):
-		"ogre":
-			$Sprite.texture = preload("res://Enemy/FixedSprites/Ogre1.png")
-			spr.look_at(target_player.position)
-			follow_speed = 30
-		"skeleton":
-			$Sprite.texture = preload("res://Enemy/FixedSprites/Skeleton1.png")
-			spr.look_at(target_player.position)
-			follow_speed = 60
 
-func _physics_process(delta: float) -> void:
-	if frozen == true:
-		return
-	if target_player:		
-		position += position.direction_to(target_player.position) * delta * follow_speed
-		var bodies = get_overlapping_bodies()
-		for body in bodies:
-			if body is Player:
-				body.health -= 1
-
-func on_Enemy_area_entered(area: Area2D):
-	if area is Fireball:
-		if area.fireMode == true:
-			print("fireMode was fire, so enemy was killed")
-			area.queue_free()
-			queue_free()
-		if area.fireMode == false:
-			print("fireMode was ice, so enemy was frozen")
-			area.queue_free()
-			frozen = true
-			$FreezeTimer.start(3)
-
-func _on_FreezeTimer_timeout():
-	frozen = false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
---
+++ WGJ136/Enemy/EnemyOld.gd
@@ -0,0 +1,59 @@
+extends Area2D
+class_name Enemy
+
+onready var player1: Player = $"../../PlayerHolder/Player1"
+onready var player2: Player = $"../../PlayerHolder/Player2"
+
+export var follow_speed: float = 30
+export var frozen: bool = false
+export var type: String = "ogre"
+export var max_health: float = 100
+
+export var health: float = max_health
+export var damage: float = 1 #damage per frame
+onready var spr: Sprite = $Sprite
+
+var target_player: Player
+
+func _ready() -> void:
+	randomize()
+	target_player = player1 if randf() > 0.5 else player2
+	connect("area_entered", self, "on_Enemy_area_entered")
+	player1.connect("died", self, "queue_free")
+	player2.connect("died", self, "queue_free")
+
+func _process(delta: float) -> void:
+	match(type):
+		"ogre":
+			$Sprite.texture = preload("res://Enemy/FixedSprites/Ogre1.png")
+			spr.look_at(target_player.position)
+			follow_speed = 30
+		"skeleton":
+			$Sprite.texture = preload("res://Enemy/FixedSprites/Skeleton1.png")
+			spr.look_at(target_player.position)
+			follow_speed = 60
+
+func _physics_process(delta: float) -> void:
+	if frozen == true:
+		return
+	if target_player:		
+		position += position.direction_to(target_player.position) * delta * follow_speed
+		var bodies = get_overlapping_bodies()
+		for body in bodies:
+			if body is Player:
+				body.health -= 1
+
+func on_Enemy_area_entered(area: Area2D):
+	if area is Fireball:
+		if area.fireMode == true:
+			print("fireMode was fire, so enemy was killed")
+			area.queue_free()
+			queue_free()
+		if area.fireMode == false:
+			print("fireMode was ice, so enemy was frozen")
+			area.queue_free()
+			frozen = true
+			$FreezeTimer.start(3)
+
+func _on_FreezeTimer_timeout():
+	frozen = false
