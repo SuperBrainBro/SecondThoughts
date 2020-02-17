06936dda3782127c4101f86f8706da19f0c04d31
SuperBrainBro <superbrainstormers@gmail.com>
2020-02-16 20:58:22 -0800

    update title screen

---
+++ WGJ136/Other/make_not_clickable.gd
@@ -0,0 +1,4 @@
+extends TextureRect
+
+func _ready():
+	self.mouse_filter = 2
--- WGJ136/Other/title_screen.gd
+++ WGJ136/Other/title_screen.gd
@@ -1,10 +1,6 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var scene_path_to_load
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,4 +9,9 @@ func _ready():
 
 
 func _on_Button_pressed(scene_to_load):
-	get_tree().change_scene(scene_to_load)
+	scene_path_to_load = scene_to_load
+	$FadeIn.show()
+	$FadeIn.fade_in()
+
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(scene_path_to_load)
