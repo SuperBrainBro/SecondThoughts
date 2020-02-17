9b5142a045de4130330e39cdcc78d0ce5a37b9ff
NoohAlavi <noohalavi@gmail.com>
2020-02-17 17:21:39 -0500

    fixed stuff but something is broken

--- WGJ136/Enemy/EnemyOld.gd
+++ WGJ136/Enemy/EnemyOld.gd
@@ -1,59 +1,71 @@
 extends Area2D
 class_name EnemyOld
 
-onready var player1: Player = $"../../PlayerHolder/Player1"
-onready var player2: Player = $"../../PlayerHolder/Player2"
+const FIREBALL_SCENE: PackedScene = preload("res://Enemy/EnemyFireball.tscn")
 
-export var follow_speed: float = 30
-export var frozen: bool = false
-export var type: String = "ogre"
-export var max_health: float = 100
+onready var player1 = $"../../PlayerHolder/Player1"
+onready var player2 = $"../../PlayerHolder/Player2"
 
-export var health: float = max_health
-export var damage: float = 1 #damage per frame
-onready var spr: Sprite = $Sprite
+export var follow_speed: float
+export var damage: float = 1
+export var health: float
+export var frozen: bool
 
-var target_player: Player
+export var is_ghost: bool
+export var is_fire_archer: bool
+
+var target_player
 
 func _ready() -> void:
 	randomize()
 	target_player = player1 if randf() > 0.5 else player2
 	connect("area_entered", self, "on_Enemy_area_entered")
+	$ShootTimer.connect("timeout", self, "shoot")
 	player1.connect("died", self, "queue_free")
 	player2.connect("died", self, "queue_free")
-
-func _process(delta: float) -> void:
-	match(type):
-		"ogre":
-			$Sprite.texture = preload("res://Enemy/FixedSprites/Ogre1.png")
-			spr.look_at(target_player.position)
-			follow_speed = 30
-		"skeleton":
-			$Sprite.texture = preload("res://Enemy/FixedSprites/Skeleton1.png")
-			spr.look_at(target_player.position)
-			follow_speed = 60
 
 func _physics_process(delta: float) -> void:
 	if frozen == true:
 		return
-	if target_player:		
+	if target_player:
+		$Sprite.look_at(target_player.position)
+		if is_fire_archer:
+			return
 		position += position.direction_to(target_player.position) * delta * follow_speed
 		var bodies = get_overlapping_bodies()
 		for body in bodies:
 			if body is Player:
-				body.health -= 1
+				body.health -= damage
+
+func _process(delta: float) -> void:
+	if health <= 0:
+		queue_free()
 
 func on_Enemy_area_entered(area: Area2D):
 	if area is Fireball:
 		if area.fireMode == true:
 			print("fireMode was fire, so enemy was killed")
-			area.queue_free()
-			queue_free()
+			if not area.can_penetrate:
+				area.queue_free()
+			if not is_fire_archer:
+				health -= 2
 		if area.fireMode == false:
 			print("fireMode was ice, so enemy was frozen")
-			area.queue_free()
+			health -= 1
+			if is_fire_archer:
+				queue_free()
+			if not area.can_penetrate:
+				area.queue_free()
 			frozen = true
 			$FreezeTimer.start(3)
-
+			
 func _on_FreezeTimer_timeout():
 	frozen = false
+
+func shoot():
+	if not is_fire_archer:
+		return
+	var fireball = FIREBALL_SCENE.instance()
+	fireball.position = position
+	fireball.direction = position.direction_to(target_player.position)
+	$"/root/Main/World/FireballHolder".add_child(fireball)
--- WGJ136/Fireball/Fireball.gd
+++ WGJ136/Fireball/Fireball.gd
@@ -10,6 +10,7 @@ export var can_penetrate: bool
 func _ready():
 	print('Bullet Created')
 	$DespawnTimer.connect("timeout", self, "despawn")
+	connect("body_entered", self, "_on_Fireball_body_entered")
 	if fireMode == true:
 		print('fireMode is 1')
 		$Sprite.texture = preload("res://Fireball/fireball.png")
@@ -25,7 +26,25 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
-	
+
+func _on_Fireball_body_entered(body: PhysicsBody2D) -> void:
+	if body is Enemy:
+		if fireMode == true:
+			print("fireMode was fire, so enemy was killed")
+			if not can_penetrate:
+				queue_free()
+			if not body.is_fire_archer:
+				body.health -= 2
+		if fireMode == false:
+			print("fireMode was ice, so enemy was frozen")
+			body.health -= 1
+			if body.is_fire_archer:
+				body.queue_free()
+			if not can_penetrate:
+				queue_free()
+			body.frozen = true
+			body.get_node("FreezeTimer").start(3)
+
 func despawn():
 	print('Destroyed Bullet')
 	queue_free()
