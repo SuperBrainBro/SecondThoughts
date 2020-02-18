e2497423ef86b8a6fbe9486301bfb04e770efa87
NoohAlavi <59208101+NoohAlavi@users.noreply.github.com>
2020-02-18 09:22:19 -0500

    Update EnemyFireball.gd

--- WGJ136/Enemy/EnemyFireball.gd
+++ WGJ136/Enemy/EnemyFireball.gd
@@ -4,8 +4,12 @@ class_name EnemyFireball
 export var direction: Vector2
 export var speed: float = 200
 
+var despawn_timer = Timer.new()
+
 func _ready() -> void:
 	connect("body_entered", self, "on_EnemyFireball_body_entered")
+	add_child(despawn_timer)
+	despawn_timer.connect("timeout", self, "queue_free")
 
 func _physics_process(delta: float) -> void:
 	position += direction * delta * speed
