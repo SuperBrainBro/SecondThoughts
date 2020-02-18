0f9076d28058c42fcef519b48f56ba13e979f411
SuperBrainBro <superbrainstormers@gmail.com>
2020-02-17 18:44:00 -0800

    k

---
+++ WGJ136/Main/SpawnRegion.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_SpawnRegion_body_entered(body):
+	pass # Replace with function body.
