a3228c15f5221c36a32ff2d38350dde900b93b5d
NoohAlavi <59208101+NoohAlavi@users.noreply.github.com>
2020-02-20 10:02:31 -0500

    Update Heart.gd

--- WGJ136/Heart/Heart/Heart.gd
+++ WGJ136/Heart/Heart/Heart.gd
@@ -5,9 +5,6 @@ func _ready() -> void:
 	connect("body_entered", self, "on_Heart_body_entered")
 
 func on_Heart_body_entered(body: PhysicsBody2D):
-	if body is Player:
-		if body.health < 100:
-			body.health = 100
-			queue_free()
-		else:
-			print("You already have full health")
+	if body.is_in_group("Players"):
+		body.health = 100
+		queue_free()
