2a6742d1af8e0e2b003b3da7c19734abed729047
SuperBrainBro <superbrainstormers@gmail.com>
2020-02-21 19:50:06 -0800

    toad

--- WGJ136/Toad/Toad.gd
+++ WGJ136/Toad/Toad.gd
@@ -12,22 +12,35 @@ var prev_pos
 var screen_size = 1280
 
 var shoot_timer: Timer = Timer.new()
+onready var val: float = 1
 
 func _ready() -> void:
-	shoot_timer.wait_time = 5.0
+	shoot_timer.wait_time = .75
 	shoot_timer.autostart = true
 	shoot_timer.connect("timeout", self, "shoot")
 	add_child(shoot_timer)
+	$AnimationPlayer.play("toadBoss")
 
-func _physics_process(delta: float) -> void:
-	velocity.x += 1
-	velocity = velocity.normalized() * speed
-	move_and_slide(velocity)
-	if position == prev_pos:
-		velocity = -velocity
-	prev_pos = position
+#func _process(delta):
+#	if raycast.is_colliding():
+#		if val == 1:
+#			raycast.cast_to = Vector2(-150, 0)
+#			val = -1
+#		elif val == -1:
+#			raycast.cast_to = Vector2(150, 0)
+#			val = 1
+#func _physics_process(_delta: float) -> void:
+#	velocity.y = 0
+#	velocity.x = 1 + speed/2
+#	velocity = Vector2(abs(velocity.x), abs(velocity.y)) * val
+#	velocity = velocity.clamped(speed)
+#	move_and_slide(velocity)
 
 func shoot():
 	var spit = SPIT.instance()
 	add_child(spit)
 	spit.direction = Vector2.UP
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$AnimationPlayer.play("toadBoss")
